850a629f911462bc268ef9a1e7f962e019ef144d
gaurakshay <gaur.akshay@gmail.com>
2018-06-07 12:39:30 -0500

    Started CreateView

---
+++ django/django-9.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title> Akshay Gaur </title>
+        <link rel="icon" href="icon.png" type="image/png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- ===============================FONTS================================== -->
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+        <!-- ================================= STYLES ================================= -->
+        <link href="../css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <!-- Testing.
+                <?php echo ("THIS IS PHP BABY"); ?> -->
+                <div class="header">
+                    <a href="http://www.akshaygaur.org/"><h1>Akshay's Blog (under construction)</h1></a>
+                </div>
+                <div class="navbar">
+                    Django Tutorial<br>
+                    &emsp;&gt;<a href="django-1.php"> Setup </a><br>
+                    &emsp;&gt;<a href="django-2.php"> App and Models </a><br>
+                    &emsp;&gt;<a href="django-3.php"> Admin Module </a><br>
+                    &emsp;&gt;<a href="django-4.php"> Media Root </a><br>
+                    &emsp;&gt;<a href="django-5.php"> Template View </a><br>
+                    &emsp;&gt;<a href="django-6.php"> List View </a><br>
+                    &emsp;&gt;<a href="django-7.php"> Details View - 1 </a><br>
+                    &emsp;&gt;<a href="django-8.php"> Details View - 2 </a><br>
+                </div>
+                <div class="content">
+                    Now that we are comfortable with using Class Based Views for
+                    ListView and DetailView, which display the data only, it is
+                    time for us to start playing with the data. On this page, 
+                    we will use Django's CreateView to add new model instances
+                    to our database.
+                </div>
+                <div class="footer">
+                    <h4>Contact me at gaur{dot}akshay{at}gmail{dot}com if you found this
+                        helpful or if you have any suggestions to improve.</h4>
+                </div>
+        </div>
+    </body>
+</html>
