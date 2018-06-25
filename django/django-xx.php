6dea5946f000bfebcbc12a4ed7e86d84688bf73d
gaurakshay <gaur.akshay@gmail.com>
2018-06-25 15:28:36 -0500

    trying hand at custom css again

---
+++ django/django-xx.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title> Akshay Gaur </title>
+  <link rel="icon" type="image/x-icon" href="../img/favicon-48.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- ===============================FONTS================================== -->
+  <!-- <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet"> -->
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans&amp;subset=devanagari,latin-ext" rel="stylesheet">
+  <!-- ================================= CSS ================================= -->
+  <link href="../css/test.css" rel="stylesheet">
+
+</head>
+<body>
+    <!-- Header Start-->
+    <header class="header">
+        <input type="checkbox" class="nav-toggle" id="nav-toggle"/>
+        <label for="nav-toggle" class="nav-ham">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M442 114H6a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6z" class=""></path></svg>
+        </label>   
+        <h1>Akshay</h1>
+    <!-- Header End-->
+      <!-- Navigation Start -->
+      <nav>
+        <ul class="nav" id="nav">
+            <li>
+                <a href="./django-1.php">Setup</a>
+            </li>
+            <li>
+                <a href="./django-2.php">App and Models</a>
+            </li>
+            <li>
+                <a href="./django-3.php">Admin Module</a>
+            </li>
+            <li>
+                <a href="./django-4.php">Media Root</a>
+            </li>
+            <li>
+                <a href="./django-5.php">Template View</a>
+            </li>
+            <li>
+                <a href="./django-6.php">List View</a>
+            </li>
+            <li>
+                <a href="./django-7.php">Detail View 1</a>
+            </li>
+            <li>
+                <a href="./django-8.php">Detail View 2</a>
+            </li>
+            <li>
+                <a href="./django-9.php">Create View</a>
+            </li>
+            <li>
+                <a href="./django-10.php">Templates</a>
+            </li>
+            <li>
+                <a href="./django-11.php">Update View</a>
+            </li>
+            <li>
+                <a href="./django-xx.php">Test CSS</a>
+            </li>
+        </ul>
+    </nav>
+    </header>
+      <!-- Navigation End -->
+      <!-- Content -->
+      <div>
+        <div style="padding-top: 70px;">
+            <h3>Update View</h3>
+            <i>WIP</i>
+        </div>
+      </div>
+</body>
+
+</html>
